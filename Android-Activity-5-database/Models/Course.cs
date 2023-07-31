7dbcd244746a772b0687837b8c19a5282d1f7a97
yuenshan <A00270323@mycambrian.ca>
2023-07-31 18:08:26 -0400

    Implemented the initial .NET MAUI Android project structure by creating a models folder and a views folder. Inside the models folder, two C# models were created to represent Student and Course entities. These models are equipped with appropriate database mapping attributes, including string. The Student class encompasses four essential fields - ID, Name, Email, and Address - while the Course class includes ID, CourseName, Professor, and Credits as key attributes. The email and course name should be unique. These models have been meticulously designed to streamline data storage and retrieval in the database, forming the backbone of the application's data management.

---
+++ Android-Activity-5-database/Models/Course.cs
@@ -0,0 +1,23 @@
+﻿using SQLite;
+
+namespace Android_Activity_5_database.Models
+{
+    // Define the table name for this class in the database
+    [Table("course")]
+    public class Course
+    {
+        // Primary key and AutoIncrement attribute to specify CourseId as the primary key with auto-incrementing values
+        [PrimaryKey, AutoIncrement]
+        public int CourseId { get; set; }
+
+        // Unique attribute to ensure that CourseName is unique in the database
+        // MaxLength attribute to set the maximum length of the CourseName to 100 characters
+        public string CourseName { get; set; }
+
+        // MaxLength attribute to set the maximum length of the Professor's name to 100 characters
+        public string Professor { get; set; }
+
+        // MaxLength attribute to set the maximum length of the Credits to 10 characters
+        public string Credits { get; set; }
+    }
+}
---
+++ Android-Activity-5-database/Models/Student.cs
@@ -0,0 +1,24 @@
+﻿using SQLite;
+
+namespace Android_Activity_5_database.Models
+{
+    // Define the table name for this class in the database
+    [Table("student")]
+    
+    public class Student
+    {
+        // Primary key and AutoIncrement attribute to specify StudentId as the primary key with auto-incrementing values
+        [PrimaryKey, AutoIncrement]
+        public int StudentId { get; set; }
+
+        // MaxLength attribute to set the maximum length of the Name to 100 characters
+        public string Name { get; set; }
+
+        // Unique attribute to ensure that Email is unique in the database
+        // MaxLength attribute to set the maximum length of the Email to 100 characters
+        public string Email { get; set; }
+
+        // MaxLength attribute to set the maximum length of the Address to 250 characters
+        public string Address { get; set; }
+    }
+}
