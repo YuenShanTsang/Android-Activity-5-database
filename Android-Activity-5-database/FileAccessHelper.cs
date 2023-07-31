c265e6bd835e5713aa03a9d704f7c3f1ef9386cd
yuenshan <A00270323@mycambrian.ca>
2023-07-31 18:15:38 -0400

    Added the delete function to enable users to remove student records from the database. Furthermore, we implemented an alert-based approach for updating student information, allowing users to modify name, email, and address fields. The Edit button triggers an input dialog, enabling users to input new values for the respective fields. We integrated error handling to validate email uniqueness, ensuring no duplicate emails are allowed among students. Successful updates trigger a success alert, and the student list is refreshed by clicking the Get All Student button. Error alerts are shown for invalid inputs and duplicate emails, improving the user experience and data integrity.

---
+++ Android-Activity-5-database/FileAccessHelper.cs
@@ -0,0 +1,17 @@
+﻿namespace Android_Activity_5_database
+{
+    public class FileAccessHelper
+    {
+        // This method returns the local file path for the specified filename
+        // The method takes a filename as input and returns the full local file path
+        // It combines the app's data directory (obtained from FileSystem.AppDataDirectory) with the filename
+        // This ensures that the file is stored in the app's local storage, specific to the platform (e.g., Android, iOS)
+        public static string GetLocalFilePath(string filename)
+        {
+            // Combine the app's data directory with the filename to get the full file path
+            // This will be a platform-specific file path (e.g., /data/user/0/com.example.app/files/filename on Android)
+            // The file will be stored in the app's local storage directory
+            return System.IO.Path.Combine(FileSystem.AppDataDirectory, filename);
+        }
+    }
+}
---
+++ Android-Activity-5-database/Views/CoursePage.xaml.cs
@@ -0,0 +1,132 @@
+﻿using System.Diagnostics;
+using Android_Activity_5_database.Models;
+
+namespace Android_Activity_5_database.Views
+{
+    public partial class CoursePage : ContentPage
+    {
+        public CoursePage()
+        {
+            InitializeComponent();
+        }
+
+        // Method to handle the click event of the "Add Course" button
+        public void OnNewButtonClicked(object sender, EventArgs args)
+        {
+            // Clear the status message text
+            statusMessage.Text = "";
+
+            try
+            {
+                // Call the AddNewCourse method from the CourseRepository to add a new course
+                App.CourseRepo.AddNewCourse(newCourse.Text, newProfessor.Text, newCredits.Text);
+                statusMessage.Text = App.CourseRepo.StatusMessage;
+            }
+            catch (Exception ex)
+            {
+                // Log or display the error message in case of failure
+                Debug.WriteLine($"Error adding new course: {ex.Message}");
+                statusMessage.Text = "Failed to add a new course.";
+            }
+        }
+
+        // Method to handle the click event of the "Edit" button in the CollectionView
+        public async void OnUpdateButtonClicked(object sender, EventArgs args)
+        {
+            // Check if the sender is a Button and if the CommandParameter is a Course object
+            if (sender is Button button && button.CommandParameter is Course course)
+            {
+                // Store the original values of the course before updating
+                string originalCourseName = course.CourseName;
+                string originalProfessor = course.Professor;
+                string originalCredits = course.Credits;
+
+                // Show input dialogs to get the new values for course name, professor, and credits
+                string newCourseName = await DisplayPromptAsync("Update Course Name", "Enter the new course name:", "OK", "Cancel", placeholder: course.CourseName);
+                string newProfessor = await DisplayPromptAsync("Update Professor", "Enter the new professor name:", "OK", "Cancel", placeholder: course.Professor);
+                string newCredits = await DisplayPromptAsync("Update Credits", "Enter the new credits:", "OK", "Cancel", placeholder: course.Credits);
+
+                // If the user cancels the update or enters empty values, do not proceed with the update
+                if (string.IsNullOrWhiteSpace(newCourseName) || string.IsNullOrWhiteSpace(newProfessor) || string.IsNullOrWhiteSpace(newCredits))
+                    return;
+
+                // Update the course object with the new values
+                course.CourseName = newCourseName;
+                course.Professor = newProfessor;
+                course.Credits = newCredits;
+
+                try
+                {
+                    // Check if the new course name already exists in the database
+                    if (!App.CourseRepo.IsCourseNameExist(course.CourseName, course.CourseId))
+                    {
+                        // Update the course in the database using the CourseRepository
+                        App.CourseRepo.UpdateCourse(course);
+
+                        // Restore the original values since the update was successful
+                        course.CourseName = originalCourseName;
+                        course.Professor = originalProfessor;
+                        course.Credits = originalCredits;
+
+                        // Show a success alert indicating the update was successful
+                        await DisplayAlert("Success", "Course information updated successfully.", "OK");
+
+                        // Refresh the course list by triggering the "Get All Courses" button click event
+                        OnGetButtonClicked(sender, args);
+                    }
+                    else
+                    {
+                        // Show an error alert indicating that the new course name already exists in the database
+                        await DisplayAlert("Error", "Course already exists.", "OK");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    // Show an error alert if the update fails
+                    await DisplayAlert("Error", $"Failed to update course: {ex.Message}", "OK");
+                }
+            }
+        }
+
+        // Method to handle the click event of the "Get All Courses" button
+        public void OnGetButtonClicked(object sender, EventArgs args)
+        {
+            // Clear the status message text
+            statusMessage.Text = "";
+
+            try
+            {
+                // Call the GetAllCourse method from the CourseRepository to retrieve all courses from the database
+                List<Course> courses = App.CourseRepo.GetAllCourse();
+                courseList.ItemsSource = courses; // Set the collection view's item source to the retrieved courses
+            }
+            catch (Exception ex)
+            {
+                // Log or display the error message in case of failure
+                Debug.WriteLine($"Error retrieving courses: {ex.Message}");
+                statusMessage.Text = "Failed to retrieve courses.";
+            }
+        }
+
+        // Method to handle the click event of the "Delete" button in the CollectionView
+        public void OnDeleteButtonClicked(object sender, EventArgs args)
+        {
+            // Check if the sender is a Button and if the CommandParameter is a Course object
+            if (sender is Button button && button.CommandParameter is Course course)
+            {
+                try
+                {
+                    // Call the DeleteCourse method from the CourseRepository to delete the selected course
+                    App.CourseRepo.DeleteCourse(course);
+                    statusMessage.Text = App.CourseRepo.StatusMessage;
+                }
+                catch (Exception ex)
+                {
+                    // Log or display the error message in case of failure
+                    Debug.WriteLine($"Error deleting course: {ex.Message}");
+                    statusMessage.Text = "Failed to delete the course.";
+                }
+            }
+        }
+    }
+}
